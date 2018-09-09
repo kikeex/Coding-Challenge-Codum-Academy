dd5bd6091d15b26a37b98c7d975b92fe6120b150
Alejandro Tarazona <31288964+Bepoom@users.noreply.github.com>
2018-09-09 17:31:45 -0500

    I reconstructed the Canvas Class
    I reconstructed the Canvas' constructor.
    I created the method to_s for Canvas.
    I added order to the code.

--- Canvas.rb
+++ Canvas.rb
@@ -1,33 +1,51 @@
+require "matrix"
+
 class Canvas
-  def C(canvas)
-    @canvasWeight=canvas[0].to_i+1
-    @canvasHight=canvas[1].to_i+1
-    toDrawCanvas
-  end
 
-  def toDrawCanvas
-    @cadena=""
-    for i in 0..@canvasHight
-      for j in 0..@canvasWeight
-        if i==0 || i==(@canvasHight)
-          @cadena+="-"
-        else
-          if j==0||j==@canvasWeight
-            @cadena+="|"
-          else
-            @cadena+=" "
-          end
-        end
-      end
-      @cadena+="\n"
+  def initialize(canvas)
+    @canvasWeight=canvas[0].to_i
+    @canvasHight=canvas[1].to_i
+    @matrix=Array.new(@canvasHight){Array.new(@canvasWeight){false}}
+  end
+  def to_s
+    puts "#{@matrix[@canvasHight-1][@canvasWeight-1].to_s}"
+    string = " " + drawFiller + drawRow(0) + "\n " +drawFiller
+    return string
+  end
+  def drawRow(row)
+    string=""
+    while(row < @canvasHight)
+      string+="\n"
+      string+="|"
+      string+=drawColumns(row)
+      string+="|"
+      row+=1
     end
-    return @cadena
+    return string
   end
-
+  def drawColumns(row)
+    col=0
+    string=""
+    while (col < @canvasWeight)
+      if((@matrix[row][col].to_s)=='true')
+        string+="X"
+      else
+        string+=" "
+      end
+      col+=1
+    end
+    return string
+  end
+  def drawFiller
+    string=""
+    for i in (1..@canvasWeight)
+      string+="-"
+    end
+    return string
+  end
   def L(x1,y1,x2,y2)
 
   end
-
   def R(x1,y1,x2,y2)
 
   end
