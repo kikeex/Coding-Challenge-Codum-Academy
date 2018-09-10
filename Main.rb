f7815e4cb5a5d0d597e0b3055698d2f1b9188293
Alejandro Tarazona <31288964+Bepoom@users.noreply.github.com>
2018-09-10 00:03:52 -0500

    Input for createLines and createRectangles
    Now, the program can read the input for createLines and createRectangles.

--- Main.rb
+++ Main.rb
@@ -1,6 +1,9 @@
 require "./Canvas"
 class Main
 
+  def initialize
+    @canvas= Canvas.new(0,0)
+  end
   def start
     puts 'enter command: '
     command=gets.chomp.to_s.split(" ")
@@ -15,18 +18,29 @@ class Main
     command.delete_at(0);
     case char
     when 'C'
-      @canvas= Canvas.new(command)
-      printCanvas
+      w=command[0].to_i
+      h=command[1].to_i
+      @canvas= Canvas.new(w,h)
     when 'L'
-
+      x1=command[0].to_i
+      y1=command[1].to_i
+      x2=command[2].to_i
+      y2=command[3].to_i
+      @canvas.createLine(x1,y1,x2,y2)
     when 'R'
-
+      x1=command[0].to_i
+      y1=command[1].to_i
+      x2=command[2].to_i
+      y2=command[3].to_i
+      @canvas.createRectangle(x1,y1,x2,y2)
     when 'B'
+
     when 'Q'
       !exit
     else
       puts 'Opcion no validad'
     end
+    printCanvas
   end
 end
 
