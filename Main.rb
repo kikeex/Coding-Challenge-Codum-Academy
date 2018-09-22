20f908314cb17d7d916b1150aa240e0afeb774ea
Johann Sebastian Ortiz Salazar <37127121+Molderck@users.noreply.github.com>
2018-09-21 22:37:17 -0500

    Updated message example for command B

--- Main.rb
+++ Main.rb
@@ -6,7 +6,7 @@ while (!menu_loop)
   puts 'Command C: Create a canvas given width and height - Example: C 20 4'
   puts 'Command L: Create a line from (x1,y1) to (x2,y2) - Example: L 1 2 6 2'
   puts 'Command R: Create a rectangle given corners - Example: R 16 1 20 3'
-  puts 'Command B: Fill the area connected to (x,y) - Example: 10 3 o'
+  puts 'Command B: Fill the area connected to (x,y) - Example: B 10 3 o'
   puts 'Command Q: Quit the program - Example: Q'
   input_text = gets.chomp
   @command.read_input(input_text)
